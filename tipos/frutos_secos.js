736973e71530bd66224d8f5c6226a2e91c4ee94c
IvanZarza <ivanzarzaestevez@gmail.com>
2024-07-02 19:38:04 +0200

    wip

--- tipos/frutos_secos.js
+++ tipos/frutos_secos.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const db = require('../db')
 
-const routerFrutoSeco = express.Router()
+const routerFrutosSecos = express.Router()
 
-routerFrutoSeco.get('/', (req, res) => {
+routerFrutosSecos.get('/', (req, res) => {
   db.query('SELECT * FROM ingredientes WHERE tipo = "fruto seco"', (err, result) => {
     if (err) {
       throw err
