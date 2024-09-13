8f57338c742ff321c247fd74bd4103c11b348234
IvanZarza <ivanzarzaestevez@gmail.com>
2024-09-13 17:06:51 +0200

    wip

--- usuario/meRecetas.js
+++ usuario/meRecetas.js
@@ -23,6 +23,28 @@ routerMeRecetas.post('/api/listadelacompra/me/:id/recetas', authMiddleware, (req
   }
 });
 
+routerMeRecetas.delete('/api/listadelacompra/me/:id/recetas/:recetaId', authMiddleware, (req, res) => {
+  try {
+    const userId = req.params.id; // Obtener el ID del usuario de la URL
+    const recetaId = req.params.recetaId; // Obtener el ID de la receta de la URL
+
+    let sql = 'DELETE FROM materias_primas.recetas WHERE id = ? AND usuarioId = ?';
+    db.query(sql, [recetaId, userId], (error, resultados) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Error interno del servidor' });
+      }
+      if (resultados.affectedRows === 0) {
+        return res.status(404).json({ mensaje: 'Receta no encontrada' });
+      }
+      res.status(200).json({ mensaje: 'Receta borrada con éxito' });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
 
 
 module.exports = routerMeRecetas;
